3418ec091cac188b653d146faefe451ce0cda33b
Aayush Shukla <aayush.shukla23@gmail.com>
2023-08-30 09:46:14 +0530

    Added the get(),set(), max() and min() functions

--- array.cpp
+++ array.cpp
@@ -26,9 +26,13 @@ class Array{
     void append_element(Array *p,int x);
     void insert_element(Array *p,int index,int x);
     int delete_element(Array *p,int index);
-    int linear_search(Array *p,int key);
-    int binary_search(Array *p, int key);
-    int recur_binary(Array *p,int l,int h,int key);
+    int linear_search(Array p,int key);
+    int binary_search(Array p, int key);
+    int recur_binary(Array p,int l,int h,int key);
+    int get(Array p,int index);
+    void set(Array *p,int index,int value);
+    int max(Array p);
+    int min(Array p);
 
 };
 void Array::display_array(){
@@ -73,10 +77,10 @@ int Array::delete_element(Array *p,int index){
    return num;
 }
 
-int Array::linear_search(Array *p,int key){
+int Array::linear_search(Array p,int key){
   bool found=false;
-  for(int i=0;i<p->length;i++){
-    if(p->aptr[i]==key){
+  for(int i=0;i<p.length;i++){
+    if(p.aptr[i]==key){
       return i;
     }
    
@@ -84,21 +88,21 @@ int Array::linear_search(Array *p,int key){
   return -1;
 }
 
-int Array::binary_search(Array *p,int key){  
+int Array::binary_search(Array p,int key){  
   bool found=false;           //Pre-requisite of binary search is that the array must be sorted
   int l=0;
-  int h=(p->length)-1;
+  int h=(p.length)-1;
   int mid;
-  while(l<=h ||p->aptr[mid]!=key){
+  while(l<=h ||p.aptr[mid]!=key){
         mid=floor((l+h)/2);
-        if(p->aptr[mid]==key){
+        if(p.aptr[mid]==key){
            return mid;
            
         }
-        else if(key<(p->aptr[mid])){
+        else if(key<(p.aptr[mid])){
            h=mid-1;
         }
-        else if(key>(p->aptr[mid])){
+        else if(key>(p.aptr[mid])){
             l=mid+1;
         }
 
@@ -106,22 +110,54 @@ int Array::binary_search(Array *p,int key){
   return -1;
 }
 
-int Array::recur_binary(Array *p,int l,int h,int key){
+int Array::recur_binary(Array p,int l,int h,int key){
   if(l<=h){
     int mid=floor((l+h/2));
-    if(p->aptr[mid]==key){
+    if(p.aptr[mid]==key){
       return mid;
     }
-    else if(key<p->aptr[mid]){
+    else if(key<p.aptr[mid]){
       return recur_binary(p,l,mid-1,key);
 
     }
-    else if(key>p->aptr[mid]){
+    else if(key>p.aptr[mid]){
       return recur_binary(p,mid+1,h,key);
     }
   }
   return -1;
 }
+
+int Array::get(Array p,int index){
+  return p.aptr[index];
+
+}
+
+void Array::set(Array *p,int index,int value){
+  p->aptr[index]=value;
+
+}
+
+int Array::max(Array p){
+  int max=p.aptr[0];
+  for(int i=1;i<p.length;i++){
+      if(p.aptr[i]>max){
+        max=p.aptr[i];
+      }
+  }
+ return max;
+}
+
+int Array::min(Array p){
+  int min=p.aptr[0];
+  for(int i=1;i<p.length;i++){
+    if(p.aptr[i]<min)
+       min=p.aptr[i];
+
+  }
+  return min;
+}
+
+
 
 int main(){
    Array A;
@@ -137,9 +173,11 @@ int main(){
    //A.display_array();
    //cout<<"The value deleted is:"<<num;
    //A.linear_search(&A,10);
-   int n=A.recur_binary(&A,0,7,13);
-   cout<<n;
-
+   //int n=A.recur_binary(&A,0,7,13);
+   //cout<<n;
+   int max,min;
+   cout<<"The max element is: "<<A.max(A)<<endl;
+   cout<<"The min element is: "<<A.min(A)<<endl;
    
 
 
