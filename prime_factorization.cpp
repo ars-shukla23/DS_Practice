fdfbf67814d3a1487e6d39b450ebd1205b9c29fd
Aayush Shukla <aayush.shukla23@gmail.com>
2023-11-03 20:02:04 +0530

    Prime Factorization of a number

---
+++ prime_factorization.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+bool is_prime(int n){
+  for(int i=2;i<n;i++){
+    if(n%2==0){
+        return false;
+    }
+  }
+  return true;
+}
+
+vector<int> prime_factorization(int n){
+    vector<int> vec;
+    int count=0;
+    for(int i=2;i<n;i++){
+        if(is_prime(i)){
+          vec.push_back(i);
+        }
+    }
+    vector<int> vec2;
+    while(n!=1){
+        while(n%vec.at(count)==0){
+            n=n/vec.at(count);
+            vec2.push_back(vec.at(count));
+        }
+        count++;
+    }  
+  return vec2;
+}
+
+int main(){
+ int n;
+ cout<<"Enter the value you want to perform prime factorization on:";
+ cin>>n;
+ vector<int> result=prime_factorization(n);
+ vector<int>::iterator it=result.begin();
+ cout<<"The prime factors of n are:";
+ for(it;it!=result.end();it++){
+    cout<<*it<<" ";
+ }
+ cout<<endl;
+
+
+    return 0;
+}
